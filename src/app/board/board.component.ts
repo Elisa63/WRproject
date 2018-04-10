972949efe47225c51cd7f4fe227c93def6ef3d0c
Elisa63 <35731079+Elisa63@users.noreply.github.com>
2018-04-10 16:02:36 +0200

    recup API
    il reste les boucles a faire pour les charts

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,6 +3,7 @@ import { DataService } from '../data.service';
 import { Chart } from 'chart.js';
 import { ChartsModule } from 'ng2-charts';
 
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -10,11 +11,11 @@ import { ChartsModule } from 'ng2-charts';
 })
 export class BoardComponent implements OnInit {
   public lineChartData:Array<any> = [
-   {data: [67, 59, 80, 81, 56, 55, 40, 30, 53, 60, 63, 70], label: 'Series A'},
-   {data: [28, 48, 40, 19, 86, 27, 90, 50, 80,55, 66, 77, 65], label: 'Series B'},
+   {data: [], label: 'Series A'},
+   {data: [], label: 'Series B'},
  ];
- public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
- 'august','september','october','november','december' ];
+ public lineChartLabels:Array<any> = ['Game 20', 'Game Type', 'Champion', 'Kill', 'Deaths', 'Assists',
+ 'Win' ];
   public lineChartOptions:any = {
     responsive: true
   };
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,15 +1,78 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+
+
+export interface Champion {
+id: number,
+name: string,
+
+}
+
+export interface Game {
+  id: number,
+  api_url: string,
+  name: string
+
+}
+
+
+
+export interface Summoner {
+  id: string,
+  summoner_name: string,
+  account_id: number,
+  profil_icon_id: string,
+  level: number,
+  league_point: number,
+  season_tier: string,
+  revision_date: DateTimeFormat[],
+  user: string,
+  summoner_in_matchs: SummonerInMatch[],
+}
+
+export interface MatchSummoner {
+  id: string,
+  game_type: string,
+  game_creation: string[],
+  participants_identities:string[]
+  summoner_in_match: string[],
+}
+
+
+export interface SummonerInMatch {
+  id: number,
+  role: string,
+  lane: string,
+  win: boolean,
+  kills: number,
+  deaths: number,
+  summoner: string,
+  match_summoner: string,
+
+}
+
+export interface Winrate {
+  summoner: Summoner,
+  win: boolean,
+  label: string,
+
+}
 
 @Injectable()
 export class DataService {
 
   constructor(private _http: HttpClient) { }
 
-/*  summonerWin() {
-    return this._http.get()
-    .map(result =>result)
+ Summoner(pseudo: string): Observable<Summoner> {
+   const httpOptions = {
+     headers: new HttpHeaders({
+       'Accept':  'application/json',
+     }),
+   };
+
+    return this._http.get<Summoner>("http://localhost/Symfony_Wrush-master/web/app_dev.php/api/summoners/"+pseudo+"/by-name", httpOptions);
   }
-*/
 }
